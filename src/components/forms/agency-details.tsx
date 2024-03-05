488f27fcf6f0d95a9e2241321e570c697018c10a
hoamx <hoamx.work@gmail.com>
2024-03-06 00:02:36 +0700

    feat: handle agency detail page and file upload

---
+++ src/components/forms/agency-details.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { Agency } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { AlertDialog } from "@/components/ui/alert-dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import FileUpload from "@/components/global/file-upload";
+
+type Props = {
+  data?: Partial<Agency>;
+};
+
+const FormSchema = z.object({
+  name: z.string().min(2, { message: "Agency name must be atleast 2 chars." }),
+  companyEmail: z.string().min(1),
+  companyPhone: z.string().min(1),
+  whiteLabel: z.boolean(),
+  address: z.string().min(1),
+  city: z.string().min(1),
+  zipCode: z.string().min(1),
+  state: z.string().min(1),
+  country: z.string().min(1),
+  agencyLogo: z.string().min(1),
+});
+
+const AgencyDetails = ({ data }: Props) => {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [deletingAgency, setDeletingAgency] = useState(false);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: data?.name,
+      companyEmail: data?.companyEmail,
+      companyPhone: data?.companyPhone,
+      whiteLabel: data?.whiteLabel || false,
+      address: data?.address,
+      city: data?.city,
+      zipCode: data?.zipCode,
+      state: data?.state,
+      country: data?.country,
+      agencyLogo: data?.agencyLogo,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  useEffect(() => {
+    if (data) {
+      form.reset(data);
+    }
+  }, [data, form]);
+
+  const handleSubmit = async () => {};
+
+  return (
+    <AlertDialog>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Agency Information</CardTitle>
+          <CardDescription>
+            Lets create an agency for you business. You can edit agency settings
+            later from the agency settings tab.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="space-y-4"
+            >
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="agencyLogo"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Agency Logo</FormLabel>
+                    <FormControl>
+                      <FileUpload
+                        apiEndpoint="agencyLogo"
+                        onChange={field.onChange}
+                        value={field.value}
+                      ></FileUpload>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </AlertDialog>
+  );
+};
+
+export default AgencyDetails;
