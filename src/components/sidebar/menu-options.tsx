a06ee5edb7b740659e7b1acb83259608058c91e7
hoamx <hoa.mx@kiotviet.com>
2024-03-08 08:15:22 +0700

    feat: add sidebar and search sub accounts feature

---
+++ src/components/sidebar/menu-options.tsx
@@ -0,0 +1,253 @@
+"use client";
+
+import {
+  Agency,
+  AgencySidebarOption,
+  SubAccount,
+  SubAccountSidebarOption,
+} from "@prisma/client";
+import React, { useEffect, useMemo, useState } from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { ChevronsUpDown, Compass, Menu, PlusCircleIcon } from "lucide-react";
+import clsx from "clsx";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+// import { useModal } from '@/providers/modal-provider'
+// import CustomModal from '@/components/global/custom-modal'
+// import SubAccountDetails from '@/components/forms/subaccount-details'
+import { Separator } from "@/components/ui/separator";
+import { icons } from "@/lib/constants";
+type Props = {
+  defaultOpen?: boolean;
+  subAccounts: SubAccount[];
+  sidebarOpt: AgencySidebarOption[] | SubAccountSidebarOption[];
+  sidebarLogo: string;
+  details: any;
+  user: any;
+  id: string;
+};
+
+const MenuOptions = ({
+  defaultOpen,
+  details,
+  id,
+  sidebarLogo,
+  sidebarOpt,
+  subAccounts,
+  user,
+}: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const openState = useMemo(
+    () => (defaultOpen ? { open: true } : {}),
+    [defaultOpen]
+  );
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return;
+  }
+
+  return (
+    <Sheet
+      modal={false}
+      // {...openState}
+      open={true}
+    >
+      <SheetTrigger
+        asChild
+        className="absolute left-4 top-4 z-[100] md:!hidden flex"
+      >
+        <Button variant="outline" size="icon">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        showX={!defaultOpen}
+        side="left"
+        className={clsx(
+          "bg-background/80 backdrop-blur-lg fixed top-0 border-r-[1px] p-6",
+          {
+            "hidden md:inline-block z-0 w-[300px]": defaultOpen,
+            "inline-block md:hidden z-[100] w-full": !defaultOpen,
+          }
+        )}
+      >
+        <div>
+          <AspectRatio ratio={16 / 5}>
+            <Image
+              src={sidebarLogo}
+              alt="Sidebar Logo"
+              fill
+              className="rounded-md object-contain"
+            />
+          </AspectRatio>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                className="w-full my-4 flex items-center justify-between py-8"
+                variant="ghost"
+              >
+                <div className="flex items-center text-left gap-2">
+                  <Compass className="" />
+                  <div className="flex flex-col">
+                    {details.name}
+                    <span className="text-muted-foreground">
+                      {details.address}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <ChevronsUpDown size={16} className="text-muted-foreground" />
+                </div>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80 h-80 mt-4 z-[200]">
+              <Command className="rounded-lg">
+                <CommandInput placeholder="Search Accounts..." />
+                <CommandList className="pb-16">
+                  <CommandEmpty>No results found</CommandEmpty>
+                  {(user?.role === "AGENCY_OWNER" ||
+                    user?.role === "AGENCY_ADMIN") &&
+                    user?.Agency && (
+                      <CommandGroup heading="Agency">
+                        <CommandItem className="!bg-transparent my-2 text-primary border-[1px] border-border p-2 rounded-md hover:!bg-muted cursor-pointer transition-all">
+                          {defaultOpen ? (
+                            <Link
+                              href={`/agency/${user?.Agency?.id}`}
+                              className="flex gap-4 w-full h-full"
+                            >
+                              <div className="relative w-16">
+                                <Image
+                                  src={user?.Agency?.agencyLogo}
+                                  alt="Agency Logo"
+                                  fill
+                                  className="rounded-md object-contain"
+                                />
+                              </div>
+                              <div className="flex flex-col flex-1">
+                                {user?.Agency?.name}
+                                <span className="text-muted-foreground">
+                                  {user?.Agency?.address}
+                                </span>
+                              </div>
+                            </Link>
+                          ) : (
+                            <SheetClose>
+                              <Link
+                                href={`/agency/${user?.Agency?.id}`}
+                                className="flex gap-4 w-full h-full"
+                              >
+                                <div className="relative w-16">
+                                  <Image
+                                    src={user?.Agency?.agencyLogo}
+                                    alt="Agency Logo"
+                                    fill
+                                    className="rounded-md object-contain"
+                                  />
+                                </div>
+                                <div className="flex flex-col flex-1">
+                                  {user?.Agency?.name}
+                                  <span className="text-muted-foreground">
+                                    {user?.Agency?.address}
+                                  </span>
+                                </div>
+                              </Link>
+                            </SheetClose>
+                          )}
+                        </CommandItem>
+                      </CommandGroup>
+                    )}
+                  <CommandGroup heading="Accounts">
+                    {!!subAccounts
+                      ? subAccounts.map((subaccount) => (
+                          <CommandItem key={subaccount.id}>
+                            {defaultOpen ? (
+                              <Link
+                                href={`/subaccount/${subaccount.id}`}
+                                className="flex gap-4 w-full h-full"
+                              >
+                                <div className="relative w-16">
+                                  <Image
+                                    src={subaccount.subAccountLogo}
+                                    alt="Subaccount Logo"
+                                    fill
+                                    className="rounded-md object-contain"
+                                  />
+                                </div>
+                                <div className="flex flex-col flex-1">
+                                  {subaccount.name}
+                                  <span className="text-muted-foreground">
+                                    {subaccount.address}
+                                  </span>
+                                </div>
+                              </Link>
+                            ) : (
+                              <SheetClose>
+                                <Link
+                                  href={`/agency/${subaccount.id}`}
+                                  className="flex gap-4 w-full h-full"
+                                >
+                                  <div className="relative w-16">
+                                    <Image
+                                      src={subaccount.subAccountLogo}
+                                      alt="Subaccount Logo"
+                                      fill
+                                      className="rounded-md object-contain"
+                                    />
+                                  </div>
+                                  <div className="flex flex-col flex-1">
+                                    {subaccount.name}
+                                    <span className="text-muted-foreground">
+                                      {subaccount.address}
+                                    </span>
+                                  </div>
+                                </Link>
+                              </SheetClose>
+                            )}
+                          </CommandItem>
+                        ))
+                      : "No Accounts found"}
+                  </CommandGroup>
+                </CommandList>
+                {(user?.role === "AGENCY_OWNER" ||
+                  user?.role === "AGENCY_ADMIN") && (
+                  <Button className="w-full flex gap-2">
+                    <PlusCircleIcon size={15} />
+                    Create Sub Account
+                  </Button>
+                )}
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MenuOptions;
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -307,3 +307,19 @@ export const upsertAgency = async (agency: Agency, price?: Plan) => {
     console.log(error);
   }
 };
+
+export const getNotificationAndUser = async (agencyId: string) => {
+  try {
+    const response = await db.notification.findMany({
+      where: { agencyId },
+      include: { User: true },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
