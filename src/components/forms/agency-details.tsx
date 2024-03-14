722c42482ae6845a1da46e94f52f51828e25f84d
hoamx <hoa.mx@kiotviet.com>
2024-03-14 13:52:01 +0700

    feat: add plan subscription

--- src/components/forms/agency-details.tsx
+++ src/components/forms/agency-details.tsx
@@ -99,7 +99,7 @@ const AgencyDetails = ({ data }: Props) => {
   const handleSubmit = async (values: z.infer<typeof FormSchema>) => {
     try {
       let newUserData;
-      let customerId;
+      let cusId;
       if (!data?.id) {
         const bodyData = {
           email: values.companyEmail,
@@ -122,36 +122,49 @@ const AgencyDetails = ({ data }: Props) => {
             state: values.zipCode,
           },
         };
+
+        const customerResponse = await fetch("/api/stripe/create-customer", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bodyData),
+        });
+
+        const customerData: { customerId: string } =
+          await customerResponse.json();
+        cusId = customerData.customerId;
       }
       // WIP: customerId
       newUserData = await initUser({
         role: "AGENCY_OWNER",
       });
 
-      if (!data?.id) {
-        await upsertAgency({
-          id: data?.id ? data.id : v4(),
-          address: values.address,
-          agencyLogo: values.agencyLogo,
-          city: values.city,
-          companyPhone: values.companyPhone,
-          country: values.country,
-          name: values.name,
-          state: values.state,
-          whiteLabel: values.whiteLabel,
-          zipCode: values.zipCode,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          companyEmail: values.companyEmail,
-          connectAccountId: "",
-          goal: 5,
-        });
-        toast({
-          title: "Created Agency",
-        });
+      if (!data?.customerId && !cusId) return;
 
-        return router.refresh();
-      }
+      const response = await upsertAgency({
+        id: data?.id ? data.id : v4(),
+        address: values.address,
+        customerId: data?.customerId || cusId || "",
+        agencyLogo: values.agencyLogo,
+        city: values.city,
+        companyPhone: values.companyPhone,
+        country: values.country,
+        name: values.name,
+        state: values.state,
+        whiteLabel: values.whiteLabel,
+        zipCode: values.zipCode,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        companyEmail: values.companyEmail,
+        connectAccountId: "",
+        goal: 5,
+      });
+      toast({
+        title: "Created Agency",
+      });
+
+      if (data?.id || response) return router.refresh();
     } catch (error) {
       console.log(error);
       toast({
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -113,3 +113,23 @@ export const ContactUserFormSchema = z.object({
   name: z.string().min(1, "Required"),
   email: z.string().email(),
 });
+
+export type Address = {
+  city: string;
+  country: string;
+  line1: string;
+  postal_code: string;
+  state: string;
+};
+
+export type ShippingInfo = {
+  address: Address;
+  name: string;
+};
+
+export type StripeCustomerType = {
+  email: string;
+  name: string;
+  shipping: ShippingInfo;
+  address: Address;
+};
